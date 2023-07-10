92e55f73770d9e624c21f13fd160643ff98edc9d
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2023-07-10 23:56:55 +0900

    java shit

---
+++ HelloWorld/DataGirl/DataGirl.java
@@ -0,0 +1,17 @@
+package DataGirl;
+public class DataGirl {
+	public static void  main(String[] args) {
+		
+		System.out.println(6); // number 6
+		System.out.println("six"); // six
+		
+		System.out.println("6"); // string 6
+		System.out.println(6+6); //12
+		
+		System.out.println(6*6); // 36
+		System.out.println("6*6"); // 6*6
+		
+		System.out.println("1111".length()); //4
+//		System.out.println(1111.length()); // string 6
+	}
+}
